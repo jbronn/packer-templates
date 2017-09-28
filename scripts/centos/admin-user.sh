72e730022874baa49ea7875df7024e01ffd5eed2
Justin Bronn <justin.bronn@digitalglobe.com>
2017-09-28 16:47:49 -0500

    s/ssh_user/admin_user/g

--- scripts/centos/admin-user.sh
+++ scripts/centos/admin-user.sh
@@ -1,30 +1,26 @@
 #!/bin/bash -e
 
-SSH_USER="${SSH_USER:-vagrant}"
-SSH_USER_AUTHORIZED_KEYS="${SSH_USER_AUTHORIZED_KEYS:-}"
-SSH_USER_PASSWORD="${SSH_USER_PASSWORD:-vagrant}"
-SSH_USER_SHELL="${SSH_USER_SHELL:-/bin/bash}"
-SSH_USER_HOME="/home/${SSH_USER}"
+ADMIN_USER="${ADMIN_USER:-vagrant}"
+ADMIN_USER_AUTHORIZED_KEYS="${ADMIN_USER_AUTHORIZED_KEYS:-}"
+ADMIN_USER_PASSWORD="${ADMIN_USER_PASSWORD:-vagrant}"
+ADMIN_USER_SHELL="${ADMIN_USER_SHELL:-/bin/bash}"
+ADMIN_USER_HOME="/home/${ADMIN_USER}"
 
-echo '--> Adding SSH user.'
-useradd \
-  -m \
-  -d ${SSH_USER_HOME} \
-  -s ${SSH_USER_SHELL} \
-  ${SSH_USER}
+echo '--> Adding admin user.'
+useradd -m -d ${ADMIN_USER_HOME} -s ${ADMIN_USER_SHELL} ${ADMIN_USER}
 
 echo '---> Setting password.'
-echo "${SSH_USER}:${SSH_USER_PASSWORD}" | chpasswd
+echo "${ADMIN_USER}:${ADMIN_USER_PASSWORD}" | chpasswd
 
 echo '---> Populating authorized_keys.'
-mkdir -pm 0700 ${SSH_USER_HOME}/.ssh
-echo "${SSH_USER_AUTHORIZED_KEYS}" > ${SSH_USER_HOME}/.ssh/authorized_keys
-chown -R ${SSH_USER}:${SSH_USER} ${SSH_USER_HOME}/.ssh
-chmod 0600 ${SSH_USER_HOME}/.ssh/authorized_keys
+mkdir -pm 0700 ${ADMIN_USER_HOME}/.ssh
+echo "${ADMIN_USER_AUTHORIZED_KEYS}" > ${ADMIN_USER_HOME}/.ssh/authorized_keys
+chown -R ${ADMIN_USER}:${ADMIN_USER} ${ADMIN_USER_HOME}/.ssh
+chmod 0600 ${ADMIN_USER_HOME}/.ssh/authorized_keys
 
-echo '---> Restoring SELinux context'
-restorecon -R ${SSH_USER_HOME}/.ssh
+echo '---> Restoring SELinux context.'
+restorecon -R ${ADMIN_USER_HOME}/.ssh
 
-echo '--> Configuring sudoer access.'
-echo "${SSH_USER} ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/${SSH_USER}
-chmod 0440 /etc/sudoers.d/${SSH_USER}
+echo '---> Configuring admin user sudo access.'
+echo "${ADMIN_USER} ALL=(ALL:ALL) NOPASSWD: ALL" > /etc/sudoers.d/${ADMIN_USER}
+chmod 0440 /etc/sudoers.d/${ADMIN_USER}
