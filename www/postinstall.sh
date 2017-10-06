f190fdbe1a5f0eaff60693a463964fdf82e962d3
Justin Bronn <justin.bronn@digitalglobe.com>
2017-10-05 21:50:07 -0500

    Make the kernel package an argument rather than an environment variable.

--- www/postinstall.sh
+++ www/postinstall.sh
@@ -3,7 +3,7 @@
 export APT_LISTBUGS_FRONTEND=none
 export APT_LISTCHANGES_FRONTEND=none
 export DEBIAN_FRONTEND=noninteractive
-export KERNEL_PACKAGE="${KERNEL_PACKAGE:-linux-generic}"
+export KERNEL_PACKAGE="${1:-linux-generic}"
 
 # No need for deb-src (2x http hits on `apt-get update`).
 sed -i /^deb-src/d /etc/apt/sources.list
@@ -11,5 +11,3 @@ sed -i /^deb-src/d /etc/apt/sources.list
 apt-get -y update
 apt-get -y -o DPkg::Options="--force-confold" upgrade
 apt-get -y install $KERNEL_PACKAGE
-
-exit 0
