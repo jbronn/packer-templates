3370b4e7fb56335aba3001e58c66648591f8d391
Justin Bronn <justin.bronn@radiantsolutions.com>
2018-06-08 17:57:00 -0500

    Add systemd unit to generate SSH keys on Ubuntu 18.04.

--- scripts/common/sshd.sh
+++ scripts/common/sshd.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -eu
 SSHD_CONFIG="${SSHD_CONFIG:-/etc/ssh/sshd_config}"
 
 # Configure SSH to:
@@ -10,6 +11,7 @@ echo '--> Configuring SSHD.'
 sed -i \
     -e 's/^\#\?\(PasswordAuthentication \(yes\|no\)\)/PasswordAuthentication no/g' \
     -e 's/^\#\?\(PermitRootLogin \(yes\|no\)\)/PermitRootLogin no/g' \
+    -e 's/^\#\?\(PubkeyAuthentication \(yes\|no\)\)/PubkeyAuthentication yes/g' \
     -e '/^HostKey \/etc\/ssh\/ssh_host_dsa_key/d' \
     -e '/^\#\?\(UseDNS \(yes\|no\)\)/d' \
     $SSHD_CONFIG
@@ -17,9 +19,12 @@ sed -i \
 # `UseDNS` config needs to be added as not available in default config on Ubuntu.
 printf "\nUseDNS no\n" >> $SSHD_CONFIG
 
-# Upstart script on Ubuntu 14.04 to automatically generate SSH keys.
-if [ "$(lsb_release -s -c)" = "trusty" ]; then
-    cat > /etc/init/ssh-keygen.conf <<-EOF
+if [ -f /etc/os-release ]; then
+    source /etc/os-release
+    if [ "$NAME" = "Ubuntu" ]; then
+        # Upstart script on Ubuntu 14.04 to automatically generate SSH keys.
+        if [ "$VERSION_CODENAME" = "trusty" ]; then
+            cat > /etc/init/ssh-keygen.conf <<EOF
 description "OpenSSH HostKey Generation"
 
 start on starting ssh
@@ -38,4 +43,29 @@ script
   fi
 end script
 EOF
+        fi
+        # Systemd unit on Ubuntu 18.04 to automatically generate SSH keys.
+        if [ "$VERSION_CODENAME" = "bionic" ]; then
+            cat > /lib/systemd/system/ssh-keygen.service <<EOF
+[Unit]
+Description=OpenSSH Server Key Generation
+ConditionFileNotEmpty=|!/etc/ssh/ssh_host_rsa_key
+ConditionFileNotEmpty=|!/etc/ssh/ssh_host_ecdsa_key
+ConditionFileNotEmpty=|!/etc/ssh/ssh_host_ed25519_key
+Before=ssh.service
+PartOf=ssh.service ssh.socket
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/ssh-keygen -A
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOF
+            # Enable ssh-keygen service.
+            systemctl daemon-reload
+            systemctl enable ssh-keygen
+        fi
+    fi
 fi
