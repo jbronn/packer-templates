d1679016532a9ed048be713af336defb9f1f5c31
Justin Bronn <justin.bronn@maxar.com>
2019-09-19 11:21:13 -0500

    Use the master branch script when provisioning for Hootenanny.

--- scripts/centos/hoot-provision.sh
+++ scripts/centos/hoot-provision.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 yum -q -y install git
-su -l vagrant -c "git clone -b develop https://github.com/ngageoint/hootenanny hoot"
+su -l vagrant -c "git clone -b master https://github.com/ngageoint/hootenanny hoot"
 su -l vagrant -c "./hoot/VagrantProvisionCentOS7.sh"
