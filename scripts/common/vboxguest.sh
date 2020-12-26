f2726b8b1a6778885046be98648afbedf7a81b2a
Justin Bronn <justin.bronn@maxar.com>
2020-12-26 15:06:47 -0700

    Shell style updates.

--- scripts/common/vboxguest.sh
+++ scripts/common/vboxguest.sh
@@ -2,27 +2,27 @@
 set -eu
 
 # Bail if guest extensions ISO wasn't uploaded.
-if [ "$VBOXGUEST" != "upload" ]; then
+if [ "${VBOXGUEST}" != "upload" ]; then
     exit 0
 fi
 
 KERNEL="$(uname -s)"
 
-if [ "$KERNEL" = "Linux" ]; then
+if [ "${KERNEL}" = "Linux" ]; then
     . /etc/os-release
 
     # Install the prerequisites necessary to build the guest extensions.
-    if [ "$ID" = "ubuntu" -o "$ID" = "debian" ]; then
+    if [ "${ID}" = "ubuntu" -o "${ID}" = "debian" ]; then
         apt-get -q -y install build-essential dkms
     fi
 
-    if [ "$ID" = "centos" -o "$ID" = "rhel" ]; then
+    if [ "${ID}" = "centos" -o "${ID}" = "rhel" ]; then
         yum install -q -y bzip2 dkms gcc kernel-devel make perl
     fi
 
     # Mount the VirtualBox guest extensions ISO.
     mkdir -p /media/cdrom
-    mount -t iso9660 -o ro -o loop $HOME/VBoxGuestAdditions.iso /media/cdrom
+    mount -t iso9660 -o ro -o loop ${HOME}/VBoxGuestAdditions.iso /media/cdrom
 
     # Install the VirtualBox guest extensions; ignore exit code
     # because no guarantee X is available.
@@ -31,7 +31,7 @@ if [ "$KERNEL" = "Linux" ]; then
     rmdir /media/cdrom
 fi
 
-if [ "$KERNEL" = "SunOS" ]; then
+if [ "${KERNEL}" = "SunOS" ]; then
     # Prevent Solaris package from asking questions interactively.
     cat > /tmp/vboxguest.admin <<EOF
 mail=
@@ -46,15 +46,15 @@ conflict=nocheck
 action=nocheck
 basedir=default
 EOF
-    VBOXGUESTDEV="$(lofiadm -a $HOME/VBoxGuestAdditions.iso)"
+    VBOXGUESTDEV="$(lofiadm -a ${HOME}/VBoxGuestAdditions.iso)"
     mkdir -p /mnt/vboxguest
-    mount -o ro -F hsfs $VBOXGUESTDEV /mnt/vboxguest
+    mount -o ro -F hsfs ${VBOXGUESTDEV} /mnt/vboxguest
     pkgadd -a /tmp/vboxguest.admin -G -d /mnt/vboxguest/VBoxSolarisAdditions.pkg all
     umount /mnt/vboxguest
     rmdir /mnt/vboxguest
     rm -f /tmp/vboxguest.admin
-    lofiadm -d $VBOXGUESTDEV
+    lofiadm -d ${VBOXGUESTDEV}
 fi
 
 # Remove ISO file.
-rm -f $HOME/VBoxGuestAdditions.iso
+rm -f ${HOME}/VBoxGuestAdditions.iso
--- scripts/openbsd/zerofree.sh
+++ scripts/openbsd/zerofree.sh
@@ -6,8 +6,8 @@ DD_BLOCKSIZE="${DD_BLOCKSIZE:-1M}"
 DD_STATUS="${DD_STATUS:-none}"
 MOUNTPOINTS="$(mount | awk '{ if ( $5 == "ffs" ) { print $3 } }')"
 
-for mountpoint in $MOUNTPOINTS; do
-    echo "---> Zero out $mountpoint."
-    dd if=/dev/zero of="$mountpoint/ffs.zero" bs=$DD_BLOCKSIZE status=$DD_STATUS || true
-    rm -f "$mountpoint/ffs.zero"
+for mountpoint in ${MOUNTPOINTS}; do
+    echo "---> Zero out ${mountpoint}."
+    dd if=/dev/zero of="${mountpoint}/ffs.zero" bs=${DD_BLOCKSIZE} status=${DD_STATUS} || true
+    rm -f "${mountpoint}/ffs.zero"
 done
